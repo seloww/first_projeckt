d1e65f53b3f7578df5498f2471a7c82fa445e66d
selow <mehmetkilic7719@gmail.com>
2022-09-19 19:48:53 +0300

    Its seloww calculator

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hi dudes")
+print("""
+
+ CALCULATOR
+
+PRESS 1 TO ADDITION
+PRESS 2 TO EXTRACTION
+PRESS 3 TO MULTIPLICATION
+PRESS 4 TO DIVISION
+
+""")
+
+islem = str(input("Choose process: "))
+
+if islem == "1":
+    sayi1 = int(input("Enter 1st number: "))
+    sayi2 = int(input("Enter 2nd number: "))
+    print("Conclusion:", sayi1 + sayi2)
+elif islem == "2":
+    sayi1 = int(input("Enter 1st number: "))
+    sayi2 = int(input("Enter 2nd number: "))
+    print("Conclusion:", sayi1 - sayi2)
+elif islem == "3":
+    sayi1 = int(input("Enter 1st number: "))
+    sayi2 = int(input("Enter 2nd number: "))
+    print("Conclusion:", sayi1 * sayi2)
+elif islem == "4":
+    sayi1 = int(input("Enter 1st number: "))
+    sayi2 = int(input("Enter 2nd number: "))
+    print("Conclusion:", sayi1/sayi2)
+else:
+    print("invalid transaction bro...")
